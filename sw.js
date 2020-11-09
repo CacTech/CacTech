4eed2d65eccc2098e9157a6ab51648bbf2cba546
IT. Orlando Hernandez (Masterk3y) <orlando.hdzcruz@gmail.com>
2020-11-09 16:00:42 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,73 +1,22 @@
-/*
- * @license
- * Your First PWA Codelab (https://g.co/codelabs/pwa)
- * Copyright 2019 Google Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License
- */
-'use strict';
-
-// CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
-
-// CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = [
-  'offline.html',
-];
-
-self.addEventListener('install', (evt) => {
-  console.log('[ServiceWorker] Install');
-// CODELAB: Precache static resources here.
-evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
-      return cache.addAll(FILES_TO_CACHE);
-    })
-);
+self.addEventListener('install', function (event) {
+  //Install now, rather than when all tabs with TodoMVC open are closed
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (evt) => {
-  console.log('[ServiceWorker] Activate');
-  // CODELAB: Remove previous cached data from disk.
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
+//Netlify doesn't expose the bundled URLs, so we have to cache by intercepting fetch requests
+//We'll assume the application is never updated and simple cache each request
+//Modified version of the code given on MDN
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (resp) {
+      //Either respond from the cache, or fetch it, cache it and then serve it
+      return resp  || fetch(event.request).then(function (response) {
+        return caches.open('v1').then(function (cache) {
+          //We have to clone the request as it can only be read once
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
     })
-);
-
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
-// CODELAB: Add fetch event handler here.
-if (evt.request.mode !== 'navigate') {
-  // Not a page navigation, bail.
-  return;
-}
-evt.respondWith(
-    fetch(evt.request)
-        .catch(() => {
-          return caches.open(CACHE_NAME)
-              .then((cache) => {
-                return cache.match('offline.html');
-              });
-        })
-);
+  )
 });
